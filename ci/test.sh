8ce4d75e8e3b1af447641677b00928f445615e2a
whitecollar <white.collar@mail.ru>
2022-08-11 10:20:51 +0000

    Update ci/test.sh

--- ci/test.sh
+++ ci/test.sh
@@ -1,1 +1,7 @@
 
+#!/bin/bash
+wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+rm packages-microsoft-prod.deb
+apt-get update &&   apt-get install -y dotnet-sdk-6.0 -y
+sudo apt-get update &&  sudo apt-get install -y aspnetcore-runtime-6.0 -y
